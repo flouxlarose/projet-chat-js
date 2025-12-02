67f22da7ea3ec64b815383ffd2de97a577285e50
felix <larosefelixb@gmail.com>
2025-12-01 19:37:06 -0500

    continuation animation pluie... trop d'itération de la pluie qui se reset

--- client/src/page-register.js
+++ client/src/page-register.js
@@ -1,4 +1,7 @@
 import {register} from './chat-api';
+let spriteList = [];
+
+
 
 window.addEventListener("load", () => {
     document.querySelector("form").onsubmit = function () {
@@ -6,4 +9,16 @@ window.addEventListener("load", () => {
             // L'enregistrement est complétée (la requête)
         });
     }
+
+    tick();
 })
+
+// Création des gouttes de pluie
+const tick = () => {
+    for(let i=0; i<10; i++){
+        let goutte = new Goutte();
+        spriteList.push(goutte);
+    }
+
+    window.requestAnimationFrame(tick); 
+}
--- client/src/sprites/Goutte.js
+++ client/src/sprites/Goutte.js
@@ -1,10 +1,10 @@
-class Pluie {
+class Goutte {
     constructor() {
         this.x = Math.random() * window.innerWidth;
         this.y = 0;
         this.speedY = Math.random() * 4 - 7; // Vitesse de chute entre -7 et -3
         this.node = document.createElement('div');
-        this.node.classList.add('pluie');
+        this.node.classList.add('goutte');
         document.body.append(this.node);
         this.length = 10 + Math.random() * 20;
         this.node.style.height = this.length + 'px';
@@ -12,12 +12,19 @@ class Pluie {
         
     }
 
+    reset(){
+        this.y = 0;
+        this.x = Math.random() * window.innerWidth;
+        this.speedY = Math.random() * 4 - 7;
+        this.node.style.left = this.x + 'px';
+    }
+
     tick(){
         this.y -= this.speedY;
         
         this.node.style.top = this.y + 'px';
         if(this.y > window.innerHeight){
-            this.node.remove();
+            this.reset();
         }
     }
 }
